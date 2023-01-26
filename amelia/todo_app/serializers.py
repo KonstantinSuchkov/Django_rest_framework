2d341568aa76d26b4844f21101030bbbc4b8fec2
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2023-01-26 19:06:02 +0300

    lesson 7

--- amelia/todo_app/serializers.py
+++ amelia/todo_app/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework.serializers import ModelSerializer
 
+from authors.serializers import AuthorModelSerializer
 from .models import Project, Todo_todo
 
 
 class ProjectModelSerializer(ModelSerializer):
+    # author = AuthorModelSerializer()
     class Meta:
         model = Project
         fields = '__all__'
 
 
 class Todo_todoModelSerializer(ModelSerializer):
+    # author = AuthorModelSerializer()
     class Meta:
         model = Todo_todo
         fields = '__all__'
